195aaf581492ca5cb04e7efea2d227ae3055799d
JJAccomando <jjaccomando.laba@solvd.com>
2024-09-08 12:37:36 -0700

    First commit

---
+++ src/main/java/com/solvd/laba/homework7/Executor.java
@@ -0,0 +1,10 @@
+package com.solvd.laba.homework7;
+
+/**
+ * Hello world!
+ */
+public class Executor {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
